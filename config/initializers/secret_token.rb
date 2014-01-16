278f580ff3f2296cebf20d925ce6e1a3e75a2a3a
Erik Hoekstra <ppppppachyderm@gmail.com>
2014-01-16 02:34:25 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MailRoomRailsExample::Application.config.secret_key_base = '6671397b5d6279b26f3932a14905572d771d7574cee13b12696e323b1eddbb3c5d05f8dc0cb0789ba20130d2af798b77435943427c2827674562c7edbcec9f5e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MailRoomRailsExample::Application.config.session_store :cookie_store, key: '_mail_room_rails_example_session'
